5a4aec49bb6c392cd64dd492ff85af0906d5b991
iyuangang <yuangang@me.com>
2017-11-10 00:48:11 +0800

    Update FSC.sh

--- FSC.sh
+++ FSC.sh
@@ -1,2 +1,137 @@
-cp ~/.vimrc ~/.vimrcbak ; cp -r ~/.vim ~/.vimbak ; rm -rf ~/.vimrc ~/.vim
-git clone https://github.com/iyuangang/.vim.git ~/.vim ; vim
+#!/usr/bin/env bash
+
+############################  SETUP PARAMETERS
+app_name='FSC.VIM'
+[ -z "$APP_PATH" ] && APP_PATH="$HOME/.vim"
+[ -z "$REPO_URI" ] && REPO_URI='https://github.com/iyuangang/.vim.git'
+[ -z "$REPO_BRANCH" ] && REPO_BRANCH='master'
+debug_mode='0'
+fork_maintainer='0'
+
+############################  BASIC SETUP TOOLS
+msg() {
+    printf '%b\n' "$1" >&2
+}
+
+success() {
+    if [ "$ret" -eq '0' ]; then
+        msg "\33[32m[✔]\33[0m ${1}${2}"
+    fi
+}
+
+error() {
+    msg "\33[31m[✘]\33[0m ${1}${2}"
+    exit 1
+}
+
+debug() {
+    if [ "$debug_mode" -eq '1' ] && [ "$ret" -gt '1' ]; then
+        msg "An error occurred in function \"${FUNCNAME[$i+1]}\" on line ${BASH_LINENO[$i+1]}, we're sorry for that."
+    fi
+}
+
+program_exists() {
+    local ret='0'
+    command -v $1 >/dev/null 2>&1 || { local ret='1'; }
+
+    # fail on non-zero return value
+    if [ "$ret" -ne 0 ]; then
+        return 1
+    fi
+
+    return 0
+}
+
+program_must_exist() {
+    program_exists $1
+
+    # throw error on non-zero return value
+    if [ "$?" -ne 0 ]; then
+        error "You must have '$1' installed to continue."
+    fi
+}
+
+variable_set() {
+    if [ -z "$1" ]; then
+        error "You must have your HOME environmental variable set to continue."
+    fi
+}
+
+lnif() {
+    if [ -e "$1" ]; then
+        ln -sf "$1" "$2"
+    fi
+    ret="$?"
+    debug
+}
+
+############################ SETUP FUNCTIONS
+
+do_backup() {
+    if [ -e "$1" ] || [ -e "$2" ] || [ -e "$3" ]; then
+        msg "Attempting to back up your original vim configuration."
+        today=`date +%Y%m%d_%s`
+        for i in "$1" "$2" "$3"; do
+            [ -e "$i" ] && [ ! -L "$i" ] && mv -v "$i" "$i.$today";
+        done
+        ret="$?"
+        success "Your original vim configuration has been backed up."
+        debug
+   fi
+}
+
+sync_repo() {
+    local repo_path="$1"
+    local repo_uri="$2"
+    local repo_branch="$3"
+    local repo_name="$4"
+
+    msg "Trying to update $repo_name"
+
+    if [ ! -e "$repo_path" ]; then
+        mkdir -p "$repo_path"
+        git clone -b "$repo_branch" "$repo_uri" "$repo_path"
+        ret="$?"
+        success "Successfully cloned $repo_name."
+    else
+        cd "$repo_path" && git pull origin "$repo_branch"
+        ret="$?"
+        success "Successfully updated $repo_name"
+    fi
+
+    debug
+}
+
+setup_plug() {
+    local system_shell="$SHELL"
+    export SHELL='/bin/sh'
+
+    vim \
+        -u "$1" \
+        "+PlugUpdate" \
+        "+qall"
+
+    export SHELL="$system_shell"
+
+    success "Now updating/installing plugins with vim-plug"
+    debug
+}
+
+############################ MAIN()
+variable_set "$HOME"
+program_must_exist "vim"
+program_must_exist "git"
+
+do_backup       "$HOME/.vim" \
+                "$HOME/.vimrc" \
+                "$HOME/.gvimrc"
+
+sync_repo       "$APP_PATH" \
+                "$REPO_URI" \
+                "$REPO_BRANCH" \
+                "$app_name"
+
+setup_plug      "$APP_PATH/vimrc"
+
+msg             "\nThanks for installing $app_name."
+msg             "© `date +%Y` http://github.com/iyuangang/.vim"
